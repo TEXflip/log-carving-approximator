ec0061c83149600779df8503d14dfa2c86d492a7
TEXflip <michele.tessari12@gmail.com>
2021-04-28 13:02:46 +0200

    added test cut on bulbasaur

--- trimeshTest.py
+++ trimeshTest.py
@@ -1,9 +1,9 @@
 import numpy as np
 import trimesh
 
-mesh = trimesh.load('3D models/cube.obj', force='mesh')
+mesh = trimesh.load('3D models/bulbasaur.STL', force='mesh')
 
-slice = mesh.section(plane_origin=mesh.centroid, 
-                     plane_normal=[0,0,1])
+mesh.invert()
+slice= trimesh.intersections.slice_mesh_plane(mesh, plane_normal=[1,0,0], plane_origin=mesh.centroid, cap=True, cached_dots=None)
 
 slice.show()
