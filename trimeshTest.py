1a7e7c559eac17cb8c2ec4c729f79654bf3a5594
TEXflip <michele.tessari12@gmail.com>
2021-05-03 16:15:35 +0200

    test taglio con 2 piani

--- trimeshTest.py
+++ trimeshTest.py
@@ -3,7 +3,18 @@ import trimesh
 
 mesh = trimesh.load('3D models/bulbasaur.STL', force='mesh')
 
-mesh.invert()
-slice= trimesh.intersections.slice_mesh_plane(mesh, plane_normal=[1,0,0], plane_origin=mesh.centroid, cap=True, cached_dots=None)
+mesh.invert() # inverte le normali
 
-slice.show()
+print(mesh.volume)
+
+# taglio con piano orizzontale
+slice1 = trimesh.intersections.slice_mesh_plane(mesh, plane_normal=[0,0,1], plane_origin=[10,0,10], cap=True, cached_dots=None)
+
+# taglio con piano verticale
+slice2 = trimesh.intersections.slice_mesh_plane(mesh, plane_normal=[1,0, 0], plane_origin=[5,0,10], cap=True, cached_dots=None)
+
+# unione dei due pezzi
+union = trimesh.boolean.union([slice1, slice2],'scad')
+
+union.show()
+
