738d9c9bd670aac5be58603be291a7358adbfe46
alezuech <zuech.ale@gmail.com>
2021-05-10 22:22:11 +0200

    Codice pulito ma sempre non funzionante

--- multiCuts.py
+++ multiCuts.py
@@ -43,6 +43,9 @@ mesh = trimesh.load('3D models/bulbasaur.STL', force='mesh')
 cubo = trimesh.load('3D models/test_cube_website.stl', force='mesh')
 
 
+# La seguente funzione è una dimostrazione di come vorrei che funzionasse il programma.
+# Purtroppo, quando applico le stesse operazioni più volte, ricevo degli errori
+
 def testMultipleCuts():
     # tagli con piani verticali
     slicev1 = trimesh.intersections.slice_mesh_plane(cubo, plane_normal=[1,0, 0], plane_origin=[2,0,0], cap=True, cached_dots=None)
@@ -57,8 +60,8 @@ def testMultipleCuts():
     # taglio 2
     union2 = trimesh.boolean.union([sliceo, slicev2], 'scad')
 
-    differenza2= cubo.difference(union2, 'scad')
-    differenza1= cubo.difference(union1, 'scad')
+    differenza2 = cubo.difference(union2, 'scad')
+    differenza1 = cubo.difference(union1, 'scad')
 
     unioneTagli = differenza1.union(differenza2, engine='scad', debug=True)
     unioneTagli.show()
@@ -69,48 +72,73 @@ def testMultipleCuts():
 
     print('Collisione fra i due volumi tagliati è: ' + str(collisionChecker.in_collision_internal()))
     
+    
+
+# Questi sono gli errori dello script: i primi 2 sono i più comuni, l'ultimo ormai non viene mai
+
+    # --- ValueError: Input mesh must be watertight to cap slice
+    # --- subprocess.CalledProcessError: Command '['/usr/bin/openscad', '/tmp/tmptj9id9ht', '-o', '/tmp/tmpdpsj2m3h.off']' returned non-zero exit status 1.
+    # --- AttributeError: 'NoneType' object has no attribute 'to_planar' 
 
 
 
-# if(cubo.is_watertight == True):
-#     print('Cubo is watertight')
-# else:
-#     print('Cubo is not watertight')
-#     trimesh.repair.fill_holes(cubo)
 
-#
 
-cuboTagliato = cubo
+def cutObject():
+
+    
+
+    if not ('cuboTagliato' in globals()):
+        global cuboTagliato
+        cuboTagliato = [cubo]
+        print('cuboTagliato initialized')
+        print('Volume of cuboTagliato is: ' + str(cuboTagliato[0].volume))
+
+
+    # tagli con piani randomici
+    slice1 = trimesh.intersections.slice_mesh_plane(cuboTagliato[-1], plane_normal=triRandomVector(), plane_origin=cuboTagliato[-1].centroid, cap=True, cached_dots=None)
+    slice2 = trimesh.intersections.slice_mesh_plane(cuboTagliato[-1], plane_normal=triRandomVector(), plane_origin=cuboTagliato[-1].centroid, cap=True, cached_dots=None)
+
+
+
+    # taglio 1 = volume tolto
+    unione = trimesh.boolean.union([slice2, slice1], 'scad')
+
+
+    # aggiorno volume rimasto
+    # in questo punto viene generata la nuova mesh non watertight
+
+    cuboTagliato.append(trimesh.boolean.difference([cuboTagliato[-1], unione], engine='scad'))
+
+
+
+    # teoricamente, i metodi di repair dovrebbero rendere una mesh watertight. Però non funzionano in questo caso
+    j = 1
+    if cuboTagliato[-1] == False:
+        while cuboTagliato[-1] == False:
+            trimesh.repair.fill_holes(cuboTagliato[-1])
+            trimesh.repair.fix_inversion(cuboTagliato[-1])
+            trimesh.repair.fix_normals(cuboTagliato[-1])
+            trimesh.repair.fix_winding(cuboTagliato[-1])
+
+
+
+
+    # inizializzo o aggiorno l'insieme dei tagli
+    if not ('unioneTagli' in globals()):
+        global unioneTagli
+        unioneTagli = unione
+    else:
+        unioneTagli = unioneTagli.union(unione, engine = 'scad', debug = True)
+    
+
+
+
+
+# è solamente una run del programma
 
 for i in range(30):
-
-    # genero 2 fette, ognuna caratterizzata da un punto random nella bounding box della mesh e da un versore random
-    slice1 = trimesh.intersections.slice_mesh_plane(cubo, plane_normal=triRandomVector(), plane_origin=triRandomOrigin(cubo), cap=True, cached_dots=None)
-    slice2 = trimesh.intersections.slice_mesh_plane(cubo, plane_normal=triRandomVector(), plane_origin=triRandomOrigin(cubo), cap=True, cached_dots=None)
-
-    # unisco le fette formando un unico taglio
-    union = trimesh.boolean.union([slice1, slice2], 'scad')
-
-    # tolgo il taglio dal volume da scolpire
-    cuboTagliato = cubo.difference(union, 'scad')
-
-    # aggiorno il volume da scolpire e la mesh totale del volume tolto
-    # se unioneTagli non è già presente, la inizializzo (casomai potrei inizializzarla solo al primo ciclo for)
-    cubo = cuboTagliato
-    if 'unioneTagli' not in globals():
-        unioneTagli = union
-    else:
-        unioneTagli = unioneTagli.union(union, engine='scad', debug=True)
-    print('Ciclo numero ' + str(i) + ' eseguito con successo')
-
-
-    # controllare che il volume da scolpire sia watertight; se non lo è, aggiustarlo
-    
-    if(cubo.is_watertight == True):
-        print('Cubo is watertight')
-    else:
-        print('Cubo is not watertight')
-        trimesh.repair.fill_holes(cubo)
+    cutObject()
 
 
 
